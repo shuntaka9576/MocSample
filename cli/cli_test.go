3098c6026212fdd59526e348a548fc98825ff24b
shuntaka9576 <shuntaka9576@gmail.com>
2018-08-25 17:22:29 +0900

    [WIP]add test cases

--- cli/cli_test.go
+++ cli/cli_test.go
@@ -1,1 +1,44 @@
 package cli_test
+
+import (
+	"bytes"
+	"testing"
+
+	"strings"
+
+	"github.com/shuntaka9576/MocSample/cli"
+)
+
+func TestCli_Run(t *testing.T) {
+	var tests = []struct {
+		pattern  string
+		name     string
+		args     []string
+		expected string
+	}{
+		//{"normal", []string{"imageConverter", "-version"}, fmt.Sprintf("imageConverter version %s", cli.Version)},
+		{"normal", "case", []string{"imageConverter", "-f", "png", "-t", "jpg", "."}, ""},
+		{"normal", "case", []string{"imageConverter", "-f", "jpg", "-t", "png", "."}, ""},
+		{"normal", "case", []string{"imageConverter", "-f", "jpg", "-t", "png", "./testdata"}, ""},
+	}
+
+	for _, tt := range tests {
+		outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
+		c := cli.NewApp(outStream, errStream)
+		if tt.pattern == "normal" {
+			if tt.pattern == "normal" {
+				t.Run(tt.name, func(t *testing.T) {
+					c.Run(tt.args)
+					if !strings.Contains(outStream.String(), tt.expected) {
+						t.Errorf("Output=%q, want %q", outStream.String(), tt.expected)
+					}
+				})
+			}
+		}
+		if tt.pattern == "non-normal" {
+			t.Run(tt.name, func(t *testing.T) {
+				c.Run(tt.args)
+			})
+		}
+	}
+}
--- converter/converter_test.go
+++ converter/converter_test.go
@@ -1,20 +1,53 @@
 package converter_test
 
 import (
-	"github.com/shuntaka9576/MocSample/imagetypes"
-	"github.com/shuntaka9576/MocSample/imagetypes/gif"
-	"github.com/shuntaka9576/MocSample/imagetypes/png"
-	"github.com/shuntaka9576/MocSample/imagetypes/jpg"
 	"testing"
+
+	"github.com/shuntaka9576/MocSample/converter"
+	_ "github.com/shuntaka9576/MocSample/imagetypes/gif"
+	_ "github.com/shuntaka9576/MocSample/imagetypes/jpg"
+	_ "github.com/shuntaka9576/MocSample/imagetypes/png"
 )
 
 func TestGetConverter(t *testing.T) {
-	//tests := []struct {
-	//	name string
-	//	input input
-	//}
-	imagetypes.ResisterImageType("png", &png.Png{})
-	imagetypes.ResisterImageType("jpg", &jpg.Jpeg{})
-	imagetypes.ResisterImageType("jpeg", &jpg.Jpeg{})
-	imagetypes.ResisterImageType("gif", &gif.Gif{})
+	type input struct {
+		from, to string
+	}
+	tests := []struct {
+		pattern string
+		name    string
+		input   input
+	}{
+		{"normal", "png to jpg", input{"png", "jpg"}},
+		{"normal", "png to jpeg", input{"png", "jpeg"}},
+		{"normal", "png to gif", input{"png", "gif"}},
+		{"normal", "gif to jpg", input{"gif", "jpg"}},
+		{"normal", "gif to jpeg", input{"gif", "jpeg"}},
+		{"normal", "gif to png", input{"gif", "png"}},
+		{"normal", "jpg to png", input{"jpg", "png"}},
+		{"normal", "jpeg to png", input{"jpeg", "png"}},
+		{"normal", "jpg to gif", input{"jpg", "gif"}},
+		{"normal", "jpeg to gif", input{"jpeg", "gif"}},
+		{"non-normal", "fail from argument", input{"jp", "gif"}},
+		{"non-normal", "fail to argument", input{"jpg", "gi"}},
+		{"non-normal", "fail all argument", input{"jp", "gi"}},
+	}
+	for _, tt := range tests {
+		if tt.pattern == "normal" {
+			t.Run(tt.name, func(t *testing.T) {
+				_, err := converter.GetConverter(tt.input.from, tt.input.to)
+				if err != nil {
+					t.Errorf("but got %v", err.Error())
+				}
+			})
+		}
+		if tt.pattern == "non-normal" {
+			t.Run(tt.name, func(t *testing.T) {
+				_, err := converter.GetConverter(tt.input.from, tt.input.to)
+				if err == nil {
+					t.Error("Test Fail")
+				}
+			})
+		}
+	}
 }
